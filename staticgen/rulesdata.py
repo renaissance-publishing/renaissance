527d7c0b0453f24136a380151b8c04a890f5f19e
Alexis Williams <alexis@typedr.at>
2019-06-23 09:23:25 -0700

    Make Rules auto-populate.

--- staticgen/rulesdata.py
+++ staticgen/rulesdata.py
@@ -1,18 +1,46 @@
 from click import secho
-from dataclasses import dataclass
+from functools import total_ordering, reduce
+from funcy.py3 import partial
 from os.path import join
-from typing import Iterator, List, Optional, Tuple
+from typing import Any, Dict, Iterator, List, Optional, Tuple
 import yaml
+gjcx
+
+@total_ordering
+class Rule(object):
+    def __init__(self, rule: Dict[str, Any]):
+        for field, ty in self.__annotations__.items():
+            try:
+                setattr(self, field, rule[field])
+            except KeyError as e:
+                if not hasattr(ty, '__args__') or not isinstance(None, ty.__args__):
+                    raise e
+                else:
+                    setattr(self, field, None)
+
+    def __lt__(self, other):
+        return reduce(lambda acc, x: acc and getattr(self, x) < getattr(other, x), self.__annotations__.keys())
+
+    def __repr__(self):
+        get = partial(getattr, self)
+        attrs = {field: get(field) for field in self.__annotations__.keys() if get(field) is not None}
+
+        return '{}({})'.format(self.__class__.__name__, attrs)
 
 
-@dataclass(order=True)
-class Skill(object):
+class Skill(Rule):
     name: str
     aptitude: str
     categories: List[str]
     description: str
     fields: Optional[str]
     specializations: Optional[str]
+
+
+class Trait(Rule):
+    name: str
+    trait_type: str
+    description: str
 
 
 class RulesData(object):
@@ -32,10 +60,7 @@ class RulesData(object):
 
                     try:
                         if rule['type'] == 'skill':
-                            self.skills.append(Skill(
-                                rule['name'], rule['aptitude'], rule['categories'], rule['description'],
-                                rule.get('fields'), rule.get('specializations')
-                            ))
+                            self.skills.append(Skill(rule))
                     except KeyError as e:
                         if e.args[0] != 'type':
                             secho(
