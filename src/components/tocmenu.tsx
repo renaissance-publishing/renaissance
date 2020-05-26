bee08d1e8ffb76b2c7c6960f951378327e90597c
Alexis Williams <alexis@typedr.at>
2020-05-26 13:21:25 -0700

    Fix URL for chapter zero

--- src/components/tocmenu.tsx
+++ src/components/tocmenu.tsx
@@ -19,6 +19,7 @@ export default () => {
                         }
                         fields {
                             slug
+                            chapter
                         }
                         frontmatter {
                             title
