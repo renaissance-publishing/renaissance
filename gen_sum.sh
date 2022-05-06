2b608327cea51709902c7a426c42f410b4e26852
David Bolding <boldingd@gmail.com>
2022-05-05 22:28:35 -0400

    copied old gen_sum, modified new gen_sum to look for proper # H1s, re-ran to get a new SUMMARY.md

--- gen_sum.sh
+++ gen_sum.sh
@@ -3,24 +3,21 @@
 printf "# Summary\\n\\n"
 
 for file in chapters/[0-9]*.md ; do
-    #echo "${file}"
-    #TITLE_LINE=`grep 'title:\(.*\)' "${file}"`
-    # one file has windows line-endings, because of course it does.
-    # and that results in a stray \r being embedded in the title.
-    # (and thanks to this SO for the one-liner: https://stackoverflow.com/questions/51995852/regular-expression-to-replace-m-with-n-or-to-remove-m)
-    TITLE_LINE=`sed -e 's/\r//' < ${file} | grep 'title:' -`
+    TITLE_LINE=`sed -e 's/\r//' < ${file} | grep '^#[ \t]\+' -m 1`
     if [ $? -ne 0 ] ; then
         echo "$file: processing error, couldn't find title line." 1>&2
         continue
     fi
-    TITLE_STRING="${TITLE_LINE#title: }"
+
+    TITLE_STRING="${TITLE_LINE#\# }"
     TITLE_STRING="${TITLE_STRING#\"}"
     TITLE_STRING="${TITLE_STRING%\"}"
-    #TITLE_STRING="${TITLE_STRING/^M//}"
+
     if [ "x${TITLE_STRING}" == "x" ] ; then
         echo "$file: title string was blank" 1>&2
         continue
     fi
+
     echo "- [${TITLE_STRING}](./${file})"
 done
 
