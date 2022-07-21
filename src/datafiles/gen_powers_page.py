1b1e25d3af506266d7892c40d3c9b61af70fcdda
David Bolding <boldingd@gmail.com>
2022-07-21 00:32:46 -0400

    lower-cased the field names, because apparently the upper-case name caused Sinestro physical pain.

--- src/datafiles/gen_powers_page.py
+++ src/datafiles/gen_powers_page.py
@@ -6,29 +6,29 @@ with open("powers.toml", "r") as ifile:
 
 # print(parsed)
 
-standard_keys = ['General', 'Name', 'Fluff', 'Requires', 'Action', 'Effect', 'Sections']
+standard_keys = ['general', 'name', 'fluff', 'requires', 'action', 'effect', 'sections']
 
-for power in sorted(parsed["Powers"], key=lambda x: x['Name']):
-    if 'General' not in power or power['General'] == False:
+for power in sorted(parsed["powers"], key=lambda x: x['name']):
+    if 'general' not in power or power['general'] == False:
         continue
 
-    sys.stdout.write(f"### {power['Name']}\n\n")
+    sys.stdout.write(f"### {power['name']}\n\n")
 
-    if "Fluff" in power:
-        sys.stdout.write(f"{power['Fluff']}\n\n")
+    if "fluff" in power:
+        sys.stdout.write(f"{power['fluff']}\n\n")
 
-    if "Requires" in power:
-        sys.stdout.write(f"- **Requires:** {power['Requires']}\n")
-    if "Action" in power:
-        sys.stdout.write(f"- **Action:** {power['Action']}\n")
-    sys.stdout.write(f"- **Effect:** {power['Effect']}\n")
-    if 'Sections' in power:
-        for section in power['Sections']:
-            sys.stdout.write(f"- **{section['Label']}:** {section['Content']}\n")
+    if "requires" in power:
+        sys.stdout.write(f"- **Requires:** {power['requires']}\n")
+    if "action" in power:
+        sys.stdout.write(f"- **Action:** {power['action']}\n")
+    sys.stdout.write(f"- **Effect:** {power['effect']}\n")
+    if 'sections' in power:
+        for section in power['sections']:
+            sys.stdout.write(f"- **{section['label']}:** {section['content']}\n")
 
     sys.stdout.write("\n")
 
     for key in power.keys():
         if key not in standard_keys:
-            sys.stderr.write(f"Power {power['Name']} contianed non-standard key {key}\n")
+            sys.stderr.write(f"Power {power['name']} contianed non-standard key {key}\n")
 
